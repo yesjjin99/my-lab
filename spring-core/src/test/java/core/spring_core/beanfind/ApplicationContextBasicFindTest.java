49a371c8ff87f5f5a4559409a70c8979e4538037
yesjjin99 <yesjjin99@naver.com>
2024-10-05 17:47:10 +0900

    Test: 스프링 빈 조회

---
+++ spring-core/src/test/java/core/spring_core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,48 @@
+package core.spring_core.beanfind;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import core.spring_core.AppConfig;
+import core.spring_core.member.MemberService;
+import core.spring_core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        // 구체 타입으로 조회하면 변경시 유연성이 떨어진다
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 실패")
+    void findBeanByNameX() {
+        // 지정한 Exception 이 터져야 테스트 성공
+        assertThrows(NoSuchBeanDefinitionException.class,
+            () -> ac.getBean("xxxxx", MemberService.class));
+    }
+}
