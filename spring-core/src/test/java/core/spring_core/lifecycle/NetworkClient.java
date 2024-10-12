e9a4ce397e6871de67961452ffe9e8b9bfdb30f8
yesjjin99 <yesjjin99@naver.com>
2024-10-12 17:24:14 +0900

    Feat: 애노테이션 @PostConstruct, @PreDestroy 적용

--- spring-core/src/test/java/core/spring_core/lifecycle/NetworkClient.java
+++ spring-core/src/test/java/core/spring_core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package core.spring_core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -26,12 +29,14 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init() {  // 빈 생성과 의존관계 주입이 끝나면 호출
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {  // 빈이 종료되기 직전 호출
         System.out.println("NetworkClient.close");
         disconnect();
