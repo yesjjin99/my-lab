6805635f8b9ee575c8e392a7c7038f5044bd5121
yesjjin99 <yesjjin99@naver.com>
2024-10-14 14:59:34 +0900

    Feat: 프로토타입 스코프

---
+++ spring-core/src/test/java/core/spring_core/scope/PrototypeTest.java
@@ -0,0 +1,45 @@
+package core.spring_core.scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        /* AnnotationConfig~ 에 클래스 지정해주면 이 클래스가 컴포넌트 스캔의 대상처럼 동작하기 때문에 스프링 빈으로 등록해버린다.
+           따라서 따로 PrototypeBean 에 @Component 추가해주지 않아도 된다 */
+
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
