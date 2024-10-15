a9f6dcbf034b46edc0b8b1afa524c0977f032fa0
yesjjin99 <yesjjin99@naver.com>
2024-10-15 15:52:00 +0900

    Feat: request 스코프와 Provider
    Provider 사용으로 request 빈을 생성하는 컨테이너에게 요청하는 시점을 지연시켜 요청이 들어온 후 생성과 의존관계 주입, 초기화까지 가능하게 한다

--- spring-core/src/main/java/core/spring_core/web/LogDemoService.java
+++ spring-core/src/main/java/core/spring_core/web/LogDemoService.java
@@ -2,15 +2,17 @@ package core.spring_core.web;
 
 import core.spring_core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
