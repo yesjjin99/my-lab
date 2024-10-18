69ad60dfd4734db4cb38c9d6fec72193c72bc606
yesjjin99 <yesjjin99@naver.com>
2024-10-18 23:37:03 +0900

    Feat: 상품 Entity - 비즈니스 로직 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -11,6 +11,7 @@ import jakarta.persistence.ManyToMany;
 import java.util.ArrayList;
 import java.util.List;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -30,4 +31,24 @@ public abstract class Item {  // 구현체를 가지고 갈 것이기 때문에 
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // == 비즈니스 로직 == //
+    // 객체 지향적으로 생각했을 때, 데이터를 가지고 있는 곳에 비즈니스 로직이 있는 것이 가장 좋다 (도메인 주도 설계)
+    /**
+     * 재고 수량 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고 수량 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity -= restStock;
+    }
 }
