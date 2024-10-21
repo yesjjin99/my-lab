9218d5dfa79f7572967ab26bf44fe86181c2821f
yesjjin99 <yesjjin99@naver.com>
2024-10-21 17:43:10 +0900

    Feat: 주문, 주문상품 엔티티 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -16,12 +16,15 @@ import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "orders")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // createOrder() 메서드를 사용할 것이기 때문에 외부에서 생성자를 사용하여 객체를 만들지 못하도록 하기 위함
 public class Order {
 
     @Id @GeneratedValue
@@ -65,5 +68,43 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // == 생성 메서드 (정적 팩토리 메서드) == //
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // == 비즈니스 로직 == //
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // == 조회 로직 == //
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems.stream()
+            .mapToInt(OrderItem::getTotalPrice)
+            .sum();
     }
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -8,11 +8,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // createOrderItem() 메서드를 사용할 것이기 때문에 외부에서 생성자를 사용하여 객체를 만들지 못하도록 하기 위함
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -29,4 +32,29 @@ public class OrderItem {
 
     private int orderPrice;  // 주문 가격
     private int count;  // 주문 수량
+
+    // == 생성 메서드 == //
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // == 비즈니스 로직 == //
+    public void cancel() {
+        item.addStock(count);
+    }
+
+    // == 조회 로직 == //
+
+    /**
+     * 주문 상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderPrice * count;
+    }
 }
