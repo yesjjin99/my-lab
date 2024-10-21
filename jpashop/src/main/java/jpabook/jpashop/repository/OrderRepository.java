3db03b78b3e9de108f0d287bc53efed25a06add9
yesjjin99 <yesjjin99@naver.com>
2024-10-21 19:00:16 +0900

    Feat: 주문 검색 기능 개발

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,9 +3,7 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import java.util.List;
 import jpabook.jpashop.domain.Order;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -22,5 +20,15 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+    // TODO: 이후 QueryDSL 로 변경 예정
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+            " where o.status = :status " +
+            " and m.name like :name", Order.class)
+            .setParameter("status", orderSearch.getOrderStatus())
+            .setParameter("name", orderSearch.getMemberName())
+//            .setFirstResult(0)  -> 페이징
+            .setMaxResults(1000)  // 최대 1000건
+            .getResultList();
+    }
 }
