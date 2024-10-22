f65c4b44721f3a7741480c9288a99bf679dd6c59
yesjjin99 <yesjjin99@naver.com>
2024-10-22 18:01:55 +0900

    Feat: 상품 등록 컨트롤러, 화면

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();  // 지금 방식보다는 static 생성자 메서드를 만들어서 의도에 맞게 생성하고 setter 를 사용하지 않는 것이 좋은 설계
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
